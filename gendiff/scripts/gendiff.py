eb55b007a6ab13436e76ab5aa72bf4099d16b28e
DemExt <xldemlx@gmail.com>
2025-08-07 00:28:37 +0300

    change scripts

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,20 @@ def main():
     parser = argparse.ArgumentParser(
         description='Генератор диффов'
     )
-    parser.add_argument('files', nargs='*', help='Файлы для сравнения')
+    
+    parser.add_argument('first_file', help='Первый файл для сравнения')
+    parser.add_argument('second_file', help='Второй файл для сравнения')
+    parser.add_argument(
+        '-f', '--format',
+        dest='format',
+        default='stylish',
+        help='set format of output'
+    )
+
     args = parser.parse_args()
 
-   
-    print("Запущен gendiff с файлами:", args.files)
+    print("Файлы:", args.first_file, args.second_file)
+    print("Формат вывода:", args.format)
+
+if __name__ == '__main__':
+    main()
